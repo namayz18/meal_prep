9e7c552d928db192cf6e278593296b3f48517ab8
namayz18 <namayz18@gmail.com>
2024-04-08 09:21:12 +1000

    calendar view reoved

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -3,9 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:meal_prep/core/extensions/theme_colors.dart';
 import 'package:meal_prep/views/home/controller/home_view_controller.dart';
 import 'package:meal_prep/views/home/view/home_day_view.dart';
-import 'package:meal_prep/views/home/view/home_month_view.dart';
-import 'package:meal_prep/views/home/view/home_week_view.dart';
-import 'package:meal_prep/views/home/widgets/calendar_segment_widget.dart';
 
 enum Calendar { day, week, month }
 
@@ -18,18 +15,10 @@ class HomeView extends StatefulWidget {
 
 class _HomeViewState extends State<HomeView> {
   final HomeViewController _homeViewController = HomeViewController();
-  Calendar _selected = Calendar.day;
   String _calendarTitle = 'Today';
   DateTime _focusDate = DateTime.now();
   final EasyInfiniteDateTimelineController _dateTimelineController =
       EasyInfiniteDateTimelineController();
-
-  // Function to handle calendar selection change
-  void _handleSegmentChange(Set<Calendar> selectedCalendars) {
-    setState(() {
-      _selected = selectedCalendars.first;
-    });
-  }
 
   void _handleOnDateChange(DateTime selectedDate) {
     setState(() {
@@ -39,13 +28,11 @@ class _HomeViewState extends State<HomeView> {
   }
 
   void _handleGoToCurrentDate() {
-    if (_selected == Calendar.day) {
-      setState(() {
-        _focusDate = DateTime.now();
-        _calendarTitle = _getCalendarTitle();
-        _dateTimelineController.animateToCurrentData();
-      });
-    }
+    setState(() {
+      _focusDate = DateTime.now();
+      _calendarTitle = _getCalendarTitle();
+      _dateTimelineController.animateToCurrentData();
+    });
   }
 
   String _getDateTimeString(DateTime date) {
@@ -61,61 +48,17 @@ class _HomeViewState extends State<HomeView> {
     return selectedDate;
   }
 
-  Widget getSelectedCalendarView() {
-    switch (_selected) {
-      case Calendar.day:
-        return HomeDayView(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: HomeDayView(
           calendarTitle: _calendarTitle,
           focusDate: _focusDate,
           onDateChange: _handleOnDateChange,
           onPressCalendar: _handleGoToCurrentDate,
           controller: _homeViewController,
           dateTimelineController: _dateTimelineController,
-        );
-      case Calendar.week:
-        return HomeWeekView(
-          focusDate: _focusDate,
-          onDateChange: _handleOnDateChange,
-          onPressCalendar: () {},
-          controller: _homeViewController,
-        );
-      case Calendar.month:
-        return HomeMonthView(
-          controller: _homeViewController,
-        );
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(80),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: Row(
-              children: [
-                const Spacer(),
-                CalendarSegmentWidget(
-                  selected: _selected,
-                  onPress: _handleSegmentChange,
-                ),
-                const Spacer(),
-              ],
-            ),
-          ),
-        ),
-        body: getSelectedCalendarView(),
-        floatingActionButton: FloatingActionButton.small(
-          heroTag: "btn1",
-          onPressed: () {},
-          backgroundColor: context.primary,
-          foregroundColor: context.onPrimary,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(60),
-          ),
-          child: const Icon(Icons.add),
         ),
       ),
     );
